98f40620e735dace8241734f2df42ba758b7e313
m-holovan <nikita2003n@gmail.com>
2023-12-03 18:00:32 +0200

    add connection to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+
+mongoose.connect(
+    `mongodb+srv://admin:${process.env.passwordForDB}@cluster0.sf13egn.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => console.log('DB Ok'))
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
